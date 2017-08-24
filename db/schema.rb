7b6f13a8385e9785686174c8f0d34270b531a18b
Vizente Freeman <vizente@leo8eleven.com>
2017-08-23 20:17:15 -0500

    updating database

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170817213915) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "devworks", force: :cascade do |t|
     t.string "client"
     t.text "image"
