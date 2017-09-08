4380c8649a637af6b314aef5a9919c23cb29dd9d
Vizente Freeman <vizente@leo8eleven.com>
2017-09-08 16:31:03 -0500

    connecting S3 to carrierwave

--- Gemfile
+++ Gemfile
@@ -5,7 +5,7 @@ git_source(:github) do |repo_name|
   "https://github.com/#{repo_name}.git"
 end
 
-
+gem 'dotenv-rails', groups: [:development, :test]
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.3'
 # Use sqlite3 as the database for Active Record
@@ -14,9 +14,9 @@ gem 'rails', '~> 5.1.3'
 gem 'pg'
 # Use Puma as the app server
 gem 'puma', '~> 3.7'
-# Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
-gem 'carrierwave', '~> 1.0'
+gem 'carrierwave'
+gem "fog-aws"
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,12 @@
+CarrierWave.configure do |config|
+  config.fog_provider = 'fog/aws'                        # required
+  config.fog_credentials = {
+    provider:              'AWS',                        # required
+    aws_access_key_id:     ENV['S3_ACCESS_ID'],                        # required
+    aws_secret_access_key: ENV['S3_SECRET_KEY'],                        # required
+    region:                ENV['S3_REGION']                   # optional, defaults to 'us-east-1'
+  }
+  config.fog_directory  = ENV['S3_BUCKET']                         # required
+  config.fog_public     = true                                        # optional, defaults to true
+  config.fog_attributes = {} # optional, defaults to {}
+end
